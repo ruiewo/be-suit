0792cf6ace9a2633ca3456d6363d9a70eb133d28
ruiewo <109674512+ruiewo@users.noreply.github.com>
2022-09-09 13:58:35 +0900

    add login page.

---
+++ pages/login/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Login from "../../components/login";
+import styles from "../../styles/Home.module.css";
+
+const LoginPage: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Login</title>
+        <meta name="description" content="login page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <Login></Login>
+      </main>
+    </div>
+  );
+};
+
+export default LoginPage;
