cef3818aa6c5f3fccc4f64fb20feacd1891fa21f
ruiewo <109674512+ruiewo@users.noreply.github.com>
2022-10-04 18:42:47 +0900

    add Prisma.

--- README.md
+++ README.md
@@ -32,3 +32,14 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+### Prisma
+
+npm install prisma --save-dev
+npx prisma init --datasource-provider sqlite
+
+// schema ファイルに Model を作成
+npx prisma migrate dev --name init
+
+// gui で見る
+npx prisma studio
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,10 +1,45 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
   name: string;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const prisma = new PrismaClient();
+
+  async function main() {
+    const user = await prisma.user.create({
+      data: {
+        name: 'Bob',
+        email: 'bob@prisma.io',
+        posts: {
+          create: {
+            title: 'Hello World',
+          },
+        },
+      },
+    });
+    console.log(user);
+
+    const usersWithPosts = await prisma.user.findMany({
+      include: {
+        posts: true,
+      },
+    });
+    console.dir(usersWithPosts, { depth: null });
+  }
+
+  main()
+    .then(async () => {
+      await prisma.$disconnect();
+    })
+    .catch(async e => {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+    });
+
   res.status(200).json({ name: 'John Doe' });
 }
