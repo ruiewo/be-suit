a42c8f74d8bc55e6a41b37eb3c1349689f02969d
ruiewo <109674512+ruiewo@users.noreply.github.com>
2022-10-14 15:02:16 +0900

    remove unused files and lines.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -27,47 +27,6 @@ async function seedEquipments() {
   await prisma.equipment.createMany({ data: data });
 }
 
-async function main() {
-  const alice = await prisma.user.upsert({
-    where: { email: 'alice@prisma.io' },
-    update: {},
-    create: {
-      email: 'alice@prisma.io',
-      name: 'Alice',
-      posts: {
-        create: {
-          title: 'Check out Prisma with Next.js',
-          content: 'https://www.prisma.io/nextjs',
-          published: true,
-        },
-      },
-    },
-  });
-  const bob = await prisma.user.upsert({
-    where: { email: 'bob@prisma.io' },
-    update: {},
-    create: {
-      email: 'bob@prisma.io',
-      name: 'Bob',
-      posts: {
-        create: [
-          {
-            title: 'Follow Prisma on Twitter',
-            content: 'https://twitter.com/prisma',
-            published: true,
-          },
-          {
-            title: 'Follow Nexus on Twitter',
-            content: 'https://twitter.com/nexusgql',
-            published: true,
-          },
-        ],
-      },
-    },
-  });
-  console.log({ alice, bob });
-}
-
 // EXECUTE
 seedEquipments()
   .then(async () => {
