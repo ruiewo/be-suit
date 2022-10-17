36e4c0ee18b602fc05d8a87e5d6a1d32ae4e99f8
ruiewo <109674512+ruiewo@users.noreply.github.com>
2022-10-17 18:33:22 +0900

    edit sign in page.

--- src/pages/signIn/index.tsx
+++ src/pages/signIn/index.tsx
@@ -1,3 +1,8 @@
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Icon from '@mui/material/Icon';
+import Typography from '@mui/material/Typography';
 import Head from 'next/head';
 import { Copyright } from '../../components/copyright';
 import SignInButton from '../../components/signInButton';
@@ -5,6 +10,8 @@ import styles from '../../styles/Home.module.css';
 import { NextPageWithLayout } from '../_app';
 
 const SignInPage: NextPageWithLayout = () => {
+  const { data: session } = useSession();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +20,25 @@ const SignInPage: NextPageWithLayout = () => {
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <SignInButton></SignInButton>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Icon sx={{ width: 120, height: 120 }}>
+              <img src="/images/logo.svg" style={{ width: 'inherit', height: 'inherit' }} />
+            </Icon>
+            <Typography component="h1" variant="h5">
+              Signed in as
+            </Typography>
+            <SignInButton></SignInButton>
+          </Box>
+        </Container>
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </main>
     </div>
