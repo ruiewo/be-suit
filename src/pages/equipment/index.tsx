05fefc10f4f0ebacfe26b7dd94600f448dd21a41
ruiewo <109674512+ruiewo@users.noreply.github.com>
2022-11-30 10:52:43 +0900

    refactoring.

--- src/pages/equipment/index.tsx
+++ src/pages/equipment/index.tsx
@@ -2,9 +2,8 @@ import type { NextPage } from 'next';
 
 import { ErrorDialog } from '../../components/errorDialog';
 import { Loading } from '../../components/loading';
-import EquipmentsTable from '../../components/table';
 import { useEquipments } from '../../hooks/useEquipments';
-import { ColumnDefinition, Details, Equipment, ValueType, convertToDisplay } from '../../models/equipment';
+import { Details, Equipment, ValueType, convertToDisplay } from '../../models/equipment';
 import styles from '../../styles/equipmentTable.module.css';
 
 const EquipmentPage: NextPage = () => {
@@ -31,28 +30,9 @@ const SearchPanel = () => {
 };
 type Props = {
   equipments: Equipment[];
-  // columns: ColumnDefinition<Details>[];
 };
 
 const Table = ({ equipments }: Props) => {
-  const equipmentBaseColumn = [
-    { key: 'id', type: 'number', label: 'ID', width: 40 },
-    { key: 'category', type: 'string', label: '管理番号', width: 100 },
-    { key: 'subCategory', type: 'string', label: '管理番号', width: 100 },
-    { key: 'categorySerial', type: 'number', label: '管理番号', width: 100 },
-    { key: 'maker', type: 'string', label: 'メーカー', width: 120 },
-    { key: 'modelNumber', type: 'string', label: '型番', width: 120 },
-    { key: 'group', type: 'string', label: '管理者', width: 120 },
-    { key: 'rentalUserStr', type: 'string', label: '使用者', width: 120 },
-    { key: 'place', type: 'string', label: '場所', width: 180 },
-    { key: 'rentalDate', type: 'date', label: '貸出日', width: 120 },
-    { key: 'returnDate', type: 'date', label: '返却日', width: 120 },
-    { key: 'registrationDate', type: 'date', label: '登録日', width: 120 },
-    { key: 'deletedDate', type: 'date', label: '削除日', width: 120 },
-    { key: 'inventoryDate', type: 'date', label: '棚卸日', width: 120 },
-    { key: 'note', type: 'string', label: '備考', width: 400 },
-  ];
-
   type ColumnDefinition<T> = {
     key: keyof T;
     type: ValueType;
