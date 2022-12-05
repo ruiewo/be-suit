b7e1f9eb46ce117dbe96dc6a39acc39500bfa0b2
ruiewo <109674512+ruiewo@users.noreply.github.com>
2022-12-05 23:42:06 +0900

    apply equipmentEditDialog to equipment page.

--- src/pages/equipment/index.tsx
+++ src/pages/equipment/index.tsx
@@ -4,6 +4,7 @@ import { ChangeEventHandler, Dispatch, SetStateAction, useEffect, useState } fro
 import { TextField } from '@mui/material';
 
 import CategoryChip from '../../components/categoryChip';
+import EquipmentEditDialog from '../../components/dialog/equipmentEditDialog';
 import { ErrorDialog } from '../../components/dialog/errorDialog';
 import { Loading } from '../../components/loading';
 import { client } from '../../models/apiClient';
@@ -21,7 +22,7 @@ const EquipmentPage: NextPage = () => {
   const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    async function get() {
+    async function load() {
       setIsLoading(true);
       try {
         const [{ equipments, columns }] = await Promise.all([
@@ -38,8 +39,10 @@ const EquipmentPage: NextPage = () => {
       }
     }
 
-    get();
+    load();
   }, [selectedCategories]);
+
+  const reload = () => setSelectedCategories([...selectedCategories]);
 
   if (isError) return <ErrorDialog />;
 
@@ -53,7 +56,7 @@ const EquipmentPage: NextPage = () => {
         selectedCategories={selectedCategories}
         setSelectedCategories={setSelectedCategories}
       />
-      {isLoading ? <Loading /> : <Table equipments={equipments} columns={columns} filterText={filterText} />}
+      {isLoading ? <Loading /> : <Table equipments={equipments} columns={columns} filterText={filterText} reload={reload} />}
     </>
   );
 };
@@ -82,11 +85,16 @@ type Props = {
   equipments: Equipment[];
   columns: ColumnDefinition<Details>[];
   filterText: string;
+  reload: () => void;
 };
 
-const Table = ({ equipments, columns, filterText }: Props) => {
+const EquipmentTable = ({ equipments, columns, filterText, reload }: Props) => {
+  return <Table equipments={equipments} columns={columns} filterText={filterText} reload={reload} />;
+};
+
+const Table = ({ equipments, columns, filterText, reload }: Props) => {
   const baseColumn: ColumnDefinition<Details>[] = [
-    { key: 'id', type: 'code', label: '管理番号', style: 'center', width: 110 },
+    { key: 'code', type: 'code', label: '管理番号', style: 'center', width: 110 },
     { key: 'maker', type: 'string', label: 'メーカー', style: 'upLeft', width: 100 },
     { key: 'modelNumber', type: 'string', label: '型番', style: 'bottomRight', width: 130 },
     { key: 'group', type: 'string', label: '管理者', style: 'upLeft', width: 100 },
@@ -98,7 +106,8 @@ const Table = ({ equipments, columns, filterText }: Props) => {
   ];
 
   const convertedEquipments = equipments.map(equipment => {
-    const converted: Record<string, string> = {};
+    const converted: Record<string, string | number> = {};
+    converted['id'] = equipment.id;
     for (const col of baseColumn) {
       converted[col.key] = convertToDisplay(equipment, col.key, col.type);
     }
@@ -115,33 +124,47 @@ const Table = ({ equipments, columns, filterText }: Props) => {
     ? convertedEquipments
     : convertedEquipments.filter(x => Object.values(x).some(value => value?.toString().toLowerCase().includes(lowerFilterText)));
 
+  const cols = [...baseColumn, ...columns];
+
+  // for dialog
+  const [equipmentId, setEquipmentId] = useState<number | null>(null);
+  const handleDialogOpen = (e: React.MouseEvent) => {
+    e.preventDefault();
+    const tr = (e.target as HTMLElement).closest<HTMLElement>('tr');
+
+    if (tr == null) {
+      return;
+    }
+
+    const targetId = parseInt(tr.dataset.id!);
+
+    setEquipmentId(targetId);
+  };
+
+  const handleDialogClose = (isEdited: boolean) => {
+    setEquipmentId(null);
+    if (isEdited) {
+      reload();
+    }
+  };
+
   return (
     <div className={styles.tableWrapper}>
       <table className={styles.table}>
         <thead className={styles.thead}>
           <tr>
-            {baseColumn.map(x => (
-              <th key={x.key} style={{ width: x.width }}>
-                {x.label}
-              </th>
-            ))}
-            {columns.map(x => (
+            {cols.map(x => (
               <th key={x.key} style={{ width: x.width }}>
                 {x.label}
               </th>
             ))}
           </tr>
         </thead>
-        <tbody className={styles.tbody}>
+        <tbody className={styles.tbody} onDoubleClick={handleDialogOpen}>
           {filteredEquipments.map(equipment => {
             return (
-              <tr key={equipment.id}>
-                {baseColumn.map(col => (
-                  <td key={col.key} data-type={col.style == null ? '' : col.style}>
-                    {equipment[col.key]}
-                  </td>
-                ))}
-                {columns.map(col => (
+              <tr key={equipment.id} data-id={equipment.id}>
+                {cols.map(col => (
                   <td key={col.key} data-type={col.style == null ? '' : col.style}>
                     {equipment[col.key]}
                   </td>
@@ -151,6 +174,7 @@ const Table = ({ equipments, columns, filterText }: Props) => {
           })}
         </tbody>
       </table>
+      {equipmentId == null ? <></> : <EquipmentEditDialog onClose={handleDialogClose} id={equipmentId} />}
     </div>
   );
 };
